c995254a3ec42848d0ec37f1250c576ade1254b7
dnguyxntuananh <dta101203@gmail.com>
2022-12-15 14:07:24 +0700

    2:07 done

--- AptechCSharpNet7/QuestionMark/Program.cs
+++ AptechCSharpNet7/QuestionMark/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-dádsadsadasdas
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+int num = 10;
+string name = "Tuấn anh";
+Console.OutputEncoding = Encoding.Unicode;
+
+// trong C# thường có 2 dạng kiểu chính
+// 1 là value type ( số thì KHÔNG được null )
+// 2 là reference type ( chuỗi, đối tượng CHO PHÉP null )
+
+string name1 = null;
+
+// code cũ, dùng để ép value type được phép null
+Nullable<int> a = null;
+
+// code mới => cho phép value type
+int? b = null;
+bool? flag = null;
+
+//
+//if(name1  != null)
+//{
+//    Console.WriteLine(true);
+//}    else
+//{
+//    Console.WriteLine(false);
+//}    
+
+// ?: => ternary operation ( toán tử 3 ngôi )
+Console.WriteLine(name1 != null ? true : false);
